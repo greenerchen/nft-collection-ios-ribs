0343b48d95bbfa07c2989889241469d183006a74
Greener Chen <greener@gmail.com>
2023-02-23 21:52:07 +0800

    misc: create and organize project files

---
+++ NFTCollection/NFTCollection/Modules/Asset Collection/AssetCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFTCollection
+//
+//  Created by Greener Chen on 2023/2/23.
+//
+
+import UIKit
+
+class AssetCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
