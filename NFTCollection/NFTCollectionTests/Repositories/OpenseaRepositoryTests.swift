f246a9b26c2a653c9da624d60f0e4a71e228a05d
Greener Chen <greener@gmail.com>
2023-04-17 13:05:52 +0800

    Extract shared test code into a helper method in OpenseaRepositoryTests

--- NFTCollection/NFTCollectionTests/Repositories/OpenseaRepositoryTests.swift
+++ NFTCollection/NFTCollectionTests/Repositories/OpenseaRepositoryTests.swift
@@ -12,63 +12,15 @@ import RxBlocking
 
 final class OpenseaRepositoryTests: XCTestCase {
 
-    func test_load_anyAssets_emitsAnyAssets() throws {
-        let exp = expectation(description: "Wait for loading")
+    func test_load_receivesAnyAssets_emitsAnyAssets() throws {
         let sut = makeSUT()
-        
-        sut.loadAssetsHandler = { loadMore in
-            return Single<AssetsResult>.create { single in
-                single(.success(AssetsResult(assets: anyAssets(), nextCursor: nil)))
-                exp.fulfill()
-                return Disposables.create()
-            }
-        }
-        
-        let result = try sut.loadAssets(loadMore: false)
-            .toBlocking(timeout: 1.0)
-            .toArray()
-        wait(for: [exp], timeout: 1.0)
-        
-        XCTAssertEqual(sut.loadAssetsCallCount, 1)
-        XCTAssertEqual(result, [AssetsResult(assets: anyAssets(), nextCursor: nil)])
+        try expect(sut, loadAssetsCallCount: 1, expectedAssets: anyAssets(), whenReceivedAssets: anyAssets())
     }
 
     func test_loadTwice_anyAssets_emitsAnyAssetsTwice() throws {
-        let exp = expectation(description: "Wait for loading")
         let sut = makeSUT()
-        
-        sut.loadAssetsHandler = { loadMore in
-            return Single<AssetsResult>.create { single in
-                single(.success(AssetsResult(assets: anyAssets(), nextCursor: nil)))
-                exp.fulfill()
-                return Disposables.create()
-            }
-        }
-        
-        let result = try sut.loadAssets(loadMore: false)
-            .toBlocking(timeout: 1.0)
-            .toArray()
-        wait(for: [exp], timeout: 1.0)
-        
-        XCTAssertEqual(sut.loadAssetsCallCount, 1)
-        XCTAssertEqual(result, [AssetsResult(assets: anyAssets(), nextCursor: nil)])
-        
-        let exp2 = expectation(description: "Wait for loading")
-        sut.loadAssetsHandler = { loadMore in
-            return Single<AssetsResult>.create { single in
-                single(.success(AssetsResult(assets: [anyAsset()], nextCursor: nil)))
-                exp2.fulfill()
-                return Disposables.create()
-            }
-        }
-        
-        let result2 = try sut.loadAssets(loadMore: true)
-            .toBlocking(timeout: 1.0)
-            .toArray()
-        wait(for: [exp2], timeout: 1.0)
-        
-        XCTAssertEqual(sut.loadAssetsCallCount, 2)
-        XCTAssertEqual(result2, [AssetsResult(assets: [anyAsset()], nextCursor: nil)])
+        try expect(sut, loadAssetsCallCount: 1, expectedAssets: anyAssets(), whenReceivedAssets: anyAssets())
+        try expect(sut, loadAssetsCallCount: 2, expectedAssets: [anyAsset()], whenReceivedAssets: [anyAsset()])
     }
     
     func test_load_failed_emitsAnError() throws {
@@ -97,4 +49,22 @@ final class OpenseaRepositoryTests: XCTestCase {
         AssetsLoadableMock()
     }
 
+    private func expect(_ sut: AssetsLoadableMock, loadAssetsCallCount: Int, expectedAssets: [Asset], whenReceivedAssets receivedAssets: [Asset]) throws {
+        let exp = expectation(description: "Wait for loading")
+        sut.loadAssetsHandler = { loadMore in
+            return Single<AssetsResult>.create { single in
+                single(.success(AssetsResult(assets: receivedAssets, nextCursor: nil)))
+                exp.fulfill()
+                return Disposables.create()
+            }
+        }
+        
+        let result = try sut.loadAssets(loadMore: false)
+            .toBlocking(timeout: 1.0)
+            .toArray()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(sut.loadAssetsCallCount, loadAssetsCallCount)
+        XCTAssertEqual(result, [AssetsResult(assets: expectedAssets, nextCursor: nil)])
+    }
 }
