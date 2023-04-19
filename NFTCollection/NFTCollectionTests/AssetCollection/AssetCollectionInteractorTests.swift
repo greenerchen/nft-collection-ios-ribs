9c5ea3eb02dacd3fd2cd9ee71818d2e6801efede
Greener Chen <greener@gmail.com>
2023-04-19 18:47:32 +0800

    Remove tests which test the RxSwift subjects

--- NFTCollection/NFTCollectionTests/AssetCollection/AssetCollectionInteractorTests.swift
+++ NFTCollection/NFTCollectionTests/AssetCollection/AssetCollectionInteractorTests.swift
@@ -59,50 +59,6 @@ final class AssetCollectionInteractorTests: XCTestCase {
         }
     }
     
-    func test_init_anyAssets_presenterPassesAnyAssets() throws {
-        let (_, _, presenter) = makeSUT(assets: anyAssets())
-        
-        let result = try presenter.assets
-            .toBlocking()
-            .first()
-        
-        XCTAssertEqual(presenter.assetsSetCallCount, 0)
-        XCTAssertEqual(result, anyAssets())
-    }
-    
-    func test_init_anyEthBalance_presenterPassesAnyEthBalance() throws {
-        let (_, _, presenter) = makeSUT(ethBalance: anyEthBalance())
-        
-        let result = try presenter.ethBalance
-            .toBlocking()
-            .first()
-        
-        XCTAssertEqual(presenter.ethBalanceSetCallCount, 0)
-        XCTAssertEqual(result, anyEthBalance())
-    }
-    
-    func test_fetchAssets_receivesAnyAssets_presenterEmitsAnyAssets() throws {
-        let exp = expectation(description: "Wait for loading")
-        let assetLoader = AssetsLoadableMock()
-        assetLoader.loadAssetsHandler = { loadMore in
-            return Single<AssetsResult>.create { single in
-                single(.success(AssetsResult(assets: anyAssets(), nextCursor: nil)))
-                exp.fulfill()
-                return Disposables.create()
-            }
-        }
-        let (interactor, _, presenter) = makeSUT(assets: emptyAssets(), assetLoader: assetLoader)
-        
-        interactor.fetchAssets(loadMore: false)
-        
-        let result = try presenter.assets
-            .toBlocking(timeout: 1.0)
-            .toArray()
-        wait(for: [exp], timeout: 1.0)
-        
-        XCTAssertEqual(result, [emptyAssets(), anyAssets()])
-    }
-    
     // MARK: - Helpers
     
     private func makeSUT(
